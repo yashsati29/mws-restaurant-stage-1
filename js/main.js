bb9e789b0352f09b3a2e267ac9591041a8e0b03d
DevS <yashsati29@gmail.com>
2018-10-18 09:14:41 +0530

    Added alt attribute to images

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ var markers = []
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt','');
   li.append(image);
 
   const name = document.createElement('h1');
