7c2297972ce3189974fb15435aa8cb882fb3e2f3
DevS <yashsati29@gmail.com>
2018-10-18 10:06:24 +0530

    Refactored heading tags

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ var markers = []
   image.setAttribute('alt','');
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute('tabindex', '0');
   li.append(name);
