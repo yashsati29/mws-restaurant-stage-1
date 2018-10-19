62351e96d6713cffc65b34dcef646585dca98b30
DevS <yashsati29@gmail.com>
2018-10-19 11:21:21 +0530

    Refactored SERVICE WORKER functionality

--- sw.js
+++ sw.js
@@ -1,45 +1,54 @@
-let cacheVersion = 'version 1';
-let cacheFiles = [
-'./',
-'./index.html',
-'./restaurant.html',
-'./js/dbhelper.js',
-'./js/main.js',
-'./js/restaurant_info.js',
-'./data/restaurants.json',
-'./css/styles.css',
-'./img/1.jpg',
-'./img/2.jpg',
-'./img/3.jpg',
-'./img/4.jpg',
-'./img/5.jpg',
-'./img/6.jpg',
-'./img/7.jpg',
-'./img/8.jpg',
-'./img/9.jpg',
-'./img/10.jpg'
-];
-/*--install a service worker--*/
-self.addEventListener('install', function(event){
-    console.log('SW installed');
+let staticCacheName = 'restaurant-static-v1';
+
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(cacheVersion)
-        .then(function(cache){
-            console.log('Files cached');
-            return cache.addAll(cacheFiles);
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './restaurant.html',
+                './css/styles.css',
+                './data/restaurants.json',
+                './js/dbhelper.js',
+                './js/main.js',
+                './js/restaurant_info.js',
+                './js/sw_register.js',
+                './img/1.jpg',
+                './img/2.jpg',
+                './img/3.jpg',
+                './img/4.jpg',
+                './img/5.jpg',
+                './img/6.jpg',
+                './img/7.jpg',
+                './img/8.jpg',
+                './img/9.jpg',
+                './img/10.jpg'
+                ]);
         })
         );
 });
-/*--Cache and return request--*/
-self.addEventListener('fetch', function(event){
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                    cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+                );
+        })
+        );
+})
+
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-        .then(function(response){
-            if(response){
-                console.log('matched', event.request.url);
-                return response;
-            }
-            return fetch(event.request);
+        .then(function(response) {
+            return response || fetch(event.request);
         })
         );
 });
